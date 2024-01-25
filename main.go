2ca80197e8a39def33b06f827e37decd670fea33
Ahmet Affan <ahmet.affan.ebcioglu@outlook.com>
2024-01-25 21:12:07 +0300

    workin on

--- main.go
+++ main.go
@@ -11,25 +11,29 @@ import (
 	"fyne.io/fyne/v2/widget"
 )
 
-func MainPageUI(label *canvas.Text, button *widget.Button) *fyne.Container {
+func MainPageUI(label *canvas.Text, line *canvas.Rectangle, button *widget.Button) *fyne.Container {
 	return container.New(layout.NewVBoxLayout(),
 		container.NewVBox(label),
+		container.NewStack(button),
 		layout.NewSpacer(),
-		container.NewVBox(button),
+		container.NewVBox(line),
 	)
 }
 
 func main() {
 	a := app.New()
-	w := a.NewWindow("Hello")
+	w := a.NewWindow("EbiQuiz")
 	w.Resize(fyne.NewSize(635, 475))
+	w.SetMainMenu(fyne.NewMainMenu())
 
 	text := canvas.NewText("Welcome To Ebitengine Quiz!!", color.NRGBA{255, 115, 22, 255})
 	text.TextSize = 30
 	text.Alignment = fyne.TextAlignCenter
 	text.TextStyle = fyne.TextStyle{Italic: false, Bold: true, Monospace: false}
 
-	w.SetContent(MainPageUI(text, widget.NewButton("Click me!", func() {
+	rect := canvas.NewRectangle(color.NRGBA{255, 115, 22, 255})
+
+	w.SetContent(MainPageUI(text, rect, widget.NewButton("Start Quiz!", func() {
 
 	})))
 
